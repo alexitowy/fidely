94c7bf69fdb11bdcd3c67f0a702430ff23a64910
Otilia Nicola <otilianicola94@gmail.com>
2024-08-21 14:06:20 +0200

    Fix: change validations forms

--- src/app/pages/forgot-password/forgot-password/forgot-password.page.ts
+++ src/app/pages/forgot-password/forgot-password/forgot-password.page.ts
@@ -11,8 +11,12 @@ export class ForgotPasswordPage implements OnInit {
   isToastOpen = false;
 
   forgotForm: FormGroup = new FormGroup({
-    email:new FormControl('', [Validators.email, Validators.required])
-  })
+    email:new FormControl('', [Validators.email, Validators.required]),
+    confirmEmail:new FormControl('', [Validators.email, Validators.required])
+  },
+{
+  validators: this.validations('email', 'confirmEmail')
+})
 
   constructor() { }
 
@@ -21,5 +25,40 @@ export class ForgotPasswordPage implements OnInit {
 
   setOpen(isOpen: boolean){
     this.isToastOpen = isOpen;
+  }
+
+  validations(email:string, confirmEmail:string){
+    return (form: FormGroup) => {
+      let currentEmail = form.get(email).value;
+      let currentConfirmEmail= form.get(confirmEmail).value;
+      if(currentEmail !== currentConfirmEmail){
+        let errors = form.get(confirmEmail).errors;
+        if(errors === null){
+          errors= {};
+        }
+        errors['match']=true;
+        form.get(confirmEmail).setErrors(errors);
+        return null;
+      }
+      if(form.get(confirmEmail).errors !== null){
+        delete form.get(confirmEmail).errors['match'];
+      }
+
+    }
+  }
+
+  errorMsgConfirmEmail(){
+    if (this.forgotForm.controls['confirmEmail'].errors?.['required']) {
+      return 'Este campo es requerido';
+    } else if (this.forgotForm.controls['confirmEmail'].errors?.['email']){
+      return 'No es un formato válido.';
+    } else {
+      return 'El correo no coincide.';
+    }
+  }
+
+  register(){
+    console.log(this.forgotForm);
+
   }
 }
--- src/app/pages/register/register/register.page.ts
+++ src/app/pages/register/register/register.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ValidationErrors,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,30 +12,120 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  registerForm: FormGroup = new FormGroup(
+    {
+      userName: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.email, Validators.required]),
+      confirmEmail: new FormControl('', [
+        Validators.email,
+        Validators.required,
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.pattern(
+          /^(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{6,}$/
+        ),
+      ]),
+      confirmPassword: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.pattern(
+          /^(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{6,}$/
+        ),
+      ]),
+      terms: new FormControl(null, Validators.requiredTrue),
+    },
+    {
+      validators: [
+        this.EqualsValidators('email', 'confirmEmail'),
+        this.EqualsValidators('password', 'confirmPassword'),
+      ],
+    }
+  );
 
-  registerForm: FormGroup = new FormGroup({
-    userName: new FormControl('', Validators.required),
-    email: new FormControl('', [Validators.email, Validators.required]),
-    confirmEmail: new FormControl('', [Validators.email, Validators.required]),
-    password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.pattern('^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,10}$')]),
-    confirmPassword: new FormControl('', [Validators.required, Validators.minLength(6), Validators.pattern('^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,10}$')]),
-    terms: new FormControl(null, Validators.requiredTrue)
-  });
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
-
-  register(){
+  register() {
     console.log(this.registerForm);
+    // this.registerForm.markAllAsTouched();
   }
 
-  checkValidationMinLength(){
-    const value =  this.registerForm.controls['password'].value;
-   //console.log(value.length);
-    //if(value.length >= 6 && value.length <= 10)
-      //console.log(true);
-    ;
+  checkValidationMinLength() {
+    const value = this.registerForm.controls['password'].value;
+    if (value.length >= 6 && value.length <= 10) {
+      return true;
+    }
+    return false;
+  }
+
+  checkValidationUppercase() {
+    const value = this.registerForm.controls['password'].value;
+
+    if (value.match(/[A-Z]/g) !== null) {
+      return true;
+    }
+    return false;
+  }
+
+  checkValidationNumber() {
+    const value = this.registerForm.controls['password'].value;
+
+    if (value.match(/\d/g) !== null) {
+      return true;
+    }
+    return false;
+  }
+
+  checkValidationCharacter() {
+    const value = this.registerForm.controls['password'].value;
+
+    if (value.match(/[#?!@$ %^&*-]/g) !== null) {
+      return true;
+    }
+    return false;
+  }
+
+  EqualsValidators(item1: string, item2: string) {
+    return (form: FormControl): ValidationErrors => {
+      console.log(form);
+      if (form.get(item1).value !== form.get(item2).value) {
+        let currentErrors = form.get(item2).errors;
+        if(currentErrors === null){
+          currentErrors = {'missMatch': true};
+        } else {
+          currentErrors['missMatch'] = true;
+        }
+        form.get(item2).setErrors(currentErrors);
+        return null;
+      }
+      console.log(form.get(item2).errors);
+      if(form.get(item2).errors !== null){
+        delete form.get(item2).errors['missMatch'];
+      }
+      return null;
+    };
+  }
+
+  getMsgErrorEmail() {
+    if (this.registerForm.controls['confirmEmail'].errors?.['required']) {
+      return 'Este campo es requerido.';
+    } else if (this.registerForm.controls['confirmEmail'].errors?.['email']) {
+      return 'No es un formato válido.';
+    } else {
+      return 'El e-mail no coincide.';
+    }
+  }
+
+  getMsgErrorPassword() {
+    if (this.registerForm.controls['confirmPassword'].errors?.['required']) {
+      return 'Este campo es requerido';
+    } else if (this.registerForm.controls['confirmPassword'].errors?.['minLength'] || this.registerForm.controls['confirmPassword'].errors?.['pattern']){
+      return 'Debe tener un mínimo de 6 a 10 caracteres, una letra mayúscula, un número y un caracter especial.';
+    } else {
+      return 'La contraseña no coincide.';
+    }
   }
 }
