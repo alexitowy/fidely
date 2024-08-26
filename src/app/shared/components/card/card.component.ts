79a0d9766dbe151b427ab223debe2e613b7490c1
Otilia Nicola <otilianicola94@gmail.com>
2024-08-26 14:48:01 +0200

    feat: funcionality card component

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,14 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
-export class CardComponent  implements OnInit {
+export class CardComponent {
+  @Input() showIcons: boolean = true;
+  @Input() card: any;
 
-  constructor() { }
+  isOpen = false;
 
-  ngOnInit() {}
+  constructor() {}
 
+  setOpen() {
+    this.isOpen = !this.isOpen;
+  }
+
+  setFavorite() {
+    this.card.favorite = !this.card.favorite;
+  }
+
+  getRange(start: number, end: number) {
+    const result = [];
+    for (let index = start; index <= end; index++) {
+      result.push(index);
+    }
+    return result;
+  }
+
+  getClass(){
+    if(+this.card.stamps.limit === 6){
+      return 'card-content-seal-3';
+    } else if(+this.card.stamps.limit === 8){
+      return 'card-content-seal-4';
+    } else {
+      return 'card-content-seal-5';
+    }
+  }
 }
