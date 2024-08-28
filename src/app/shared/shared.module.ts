fd23f09fdffb933c5fef959faa399a787b35874d
Otilia Nicola <otilianicola94@gmail.com>
2024-08-28 13:33:20 +0200

    feat: add component card-account and routerlink

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,6 +8,8 @@ import { CardComponent } from './components/card/card.component';
 import { OverviewComponent } from './components/overview/overview.component';
 import { GalleryComponent } from './components/gallery/gallery.component';
 import { BondsComponent } from './components/bonds/bonds.component';
+import { CardAccountComponent } from './components/card-account/card-account.component';
+import { RouterLink } from '@angular/router';
 
 
 
@@ -18,7 +20,8 @@ import { BondsComponent } from './components/bonds/bonds.component';
     CardComponent,
     OverviewComponent,
     GalleryComponent,
-    BondsComponent
+    BondsComponent,
+    CardAccountComponent
   ],
   exports: [
     HeaderComponent,
@@ -27,13 +30,15 @@ import { BondsComponent } from './components/bonds/bonds.component';
     CardComponent,
     OverviewComponent,
     GalleryComponent,
-    BondsComponent
+    BondsComponent,
+    CardAccountComponent
   ],
   imports: [
     CommonModule,
     IonicModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    RouterLink
   ]
 })
 export class SharedModule { }
