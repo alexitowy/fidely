97d1effd69f625af67fbb3b550f708e3b3d7e44f
Otilia Nicola <otilianicola94@gmail.com>
2024-08-29 11:55:58 +0200

    feat: add img profile, formGroup and structure html

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+
+  profileForm: FormGroup = new FormGroup(
+    {
+    email: new FormControl('', [Validators.email, Validators.required]),
+
+    dateBirth: new FormControl('', Validators.required),
+
+    phone: new FormControl('',[Validators.required, Validators.pattern(/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/g)]),
+  });
 
   constructor() { }
 
