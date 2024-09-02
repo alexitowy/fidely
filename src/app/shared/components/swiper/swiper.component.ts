68630885bead7c0d93cf6886da2bb80a04ecd262
Otilia Nicola <otilianicola94@gmail.com>
2024-09-02 17:26:50 +0200

    feat: crear component swiper

---
+++ src/app/shared/components/swiper/swiper.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { DataSwiper } from 'src/app/core/interfaces/dataSwiper.interface';
+import { SwiperOptions } from 'swiper/types';
+
+@Component({
+  selector: 'app-swiper',
+  templateUrl: './swiper.component.html',
+  styleUrls: ['./swiper.component.scss'],
+})
+export class SwiperComponent  implements OnInit {
+
+  @ViewChild('swiper', {static:true}) swiper;
+
+  @Input() swiperParams: SwiperOptions = {
+    slidesPerView: 1,
+  };
+
+  @Input() dataImg: DataSwiper [];
+
+  constructor() { }
+
+  ngOnInit() {
+    Object.assign(this.swiper.nativeElement, this.swiperParams);
+    this.swiper.nativeElement.initialize();
+  }
+
+}
