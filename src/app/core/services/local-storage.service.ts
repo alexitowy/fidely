8d187a7408f990e25f35e841923eb16c73d07569
Otilia Nicola <otilianicola94@gmail.com>
2024-09-03 17:08:18 +0200

    feat: add ls service

---
+++ src/app/core/services/local-storage.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Preferences } from '@capacitor/preferences';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LocalStorageService {
+  constructor() {}
+
+  async set(key: string, value: any) {
+    await Preferences.set({
+      key,
+      value: JSON.stringify(value),
+    });
+  }
+
+  async get(key: string) {
+    const item = await Preferences.get({ key });
+    if (item && item.value) {
+      return JSON.parse(item.value as string);
+    } else {
+      return null;
+    }
+  }
+
+  async remove(key: string) {
+    await Preferences.remove({ key });
+  }
+
+  removeItems(key: string[]) {
+    key.forEach(async (item) => await this.remove(item));
+  }
+}
