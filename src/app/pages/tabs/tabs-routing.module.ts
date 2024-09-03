a6c0fa853de6a1c35b2351e3a41f8523103129c0
Otilia Nicola <otilianicola94@gmail.com>
2024-09-03 12:04:22 +0200

    feat: add ruote by view-search

--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -24,6 +24,10 @@ const routes: Routes = [
       {
         path: 'profile',
         loadChildren: () => import('../profile/profile.module').then( m => m.ProfilePageModule)
+      },
+      {
+        path: 'view-search',
+        loadChildren: () => import('../view-search/view-search.module').then( m => m.ViewSearchPageModule)
       }
     ]
   }
