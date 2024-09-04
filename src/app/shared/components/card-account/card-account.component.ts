bcbac3f4925c0ca935285a6342fb5b8878f4325f
Otilia Nicola <otilianicola94@gmail.com>
2024-09-04 11:16:41 +0200

    feat: implement function redirec to

--- src/app/shared/components/card-account/card-account.component.ts
+++ src/app/shared/components/card-account/card-account.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { FirebaseAuthenticationService } from 'src/app/core/services/firebase-authentication.service';
 
 @Component({
   selector: 'app-card-account',
@@ -9,8 +11,17 @@ export class CardAccountComponent  implements OnInit {
 
   @Input() data: any;
 
-  constructor() { }
+  constructor(
+    private readonly navCtrl: NavController,
+    private firebaseAuthService: FirebaseAuthenticationService
+  ) {}
 
   ngOnInit() {}
 
+  async redirectTo(){
+    if(this.data.url === '/login'){
+     await this.firebaseAuthService.signOut();
+    }
+  this.navCtrl.navigateForward(this.data.url);
+  }
 }
