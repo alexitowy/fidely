11591a5003b0cce856907ed5facac5cf52fd5198
Otilia Nicola <otilianicola94@gmail.com>
2024-09-04 14:15:51 +0200

    feat: add share function

--- src/app/pages/details/details.page.ts
+++ src/app/pages/details/details.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Share } from '@capacitor/share';
 
 @Component({
   selector: 'app-details',
@@ -13,10 +14,19 @@ export class DetailsPage implements OnInit {
 
   ngOnInit() {
   }
-
-
   handlerChange(event: any){
     this.viewSelected = event.detail.value;
     console.log(this.viewSelected);
+  }
+
+  async share(){
+    console.log(await Share.canShare());
+
+     await Share.share({
+      title: 'test',
+      text: 'test',
+      url: 'https://maps.app.goo.gl/KnEHFvo8GiyVDgNn7',
+      dialogTitle: 'testing'
+    });
   }
 }
