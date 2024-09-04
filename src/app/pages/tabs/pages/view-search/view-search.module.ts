359b56acab7690aa0fa89e2d9c3ebe2b9fbf0b3c
Otilia Nicola <otilianicola94@gmail.com>
2024-09-04 14:15:26 +0200

    feat: add view-search componet

--- src/app/pages/tabs/pages/view-search/view-search.module.ts
+++ src/app/pages/tabs/pages/view-search/view-search.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { ViewSearchPageRoutingModule } from './view-search-routing.module';
 
 import { ViewSearchPage } from './view-search.page';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ViewSearchPageRoutingModule
+    ViewSearchPageRoutingModule,
+    SharedModule
   ],
   declarations: [ViewSearchPage]
 })
