8240b3f2f3aa622db875881760c4f022673b04e5
Otilia Nicola <otilianicola94@gmail.com>
2024-09-06 13:02:20 +0200

    feat: create dataCard

--- src/app/core/interfaces/dataCard.interface.ts
+++ src/app/core/interfaces/dataCard.interface.ts
@@ -1,22 +1,31 @@
-export interface DataCard{
-
-    id: string,
-    icon: string,
-    title: string,
-    subtitle: string,
-    currentCountService: string,
-    maxCountService: string,
-    award: string,
-    favorite: boolean,
-    categoryName: string,
-    categoryColor: string,
-    stamps: Stamps,
-    addCard?: boolean
+export interface DataCard {
+  id: string;
+  icon: string;
+  title: string;
+  subtitle: string;
+  desc: string;
+  currentCountService: string;
+  maxCountService: string;
+  award: string;
+  favorite: boolean;
+  categoryName: string;
+  categoryColor: string;
+  stamps: Stamps;
+  addCard?: boolean;
 }
 
-export interface Stamps{
-    limit: string,
-    complete: string,
-    imgComplete:string,
-    imgDefault:string
+export interface Stamps {
+  limit: string;
+  complete: string;
+  imgComplete: string;
+  imgDefault: string;
+}
+
+export interface DataCardInfo {
+  id: string;
+  icon: string;
+  title: string;
+  desc: string;
+  categoryName: string;
+  categoryColor: string;
 }
