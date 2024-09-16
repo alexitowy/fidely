eb3015019a076022b8795a67739011c01766c6d9
Alex Guerrero <alexitowy@gmail.com>
2024-09-16 16:27:19 +0200

    fix: import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { FirebaseAuthenticationService } from './core/services/firebase-authentication.service';
 import { FirebaseRemoteConfigService } from './core/services/firebase-remote-config.service';
 import { environment } from 'src/environments/environment';
-import * as packageJson from '../../package.json';
+import packageJson from '../../package.json';
 
 @Component({
   selector: 'app-root',
