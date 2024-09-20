af6f597762145bb232388d7e92564dfc1398cfd4
Otilia Nicola <otilianicola94@gmail.com>
2024-09-20 14:53:41 +0200

    fix: actualization

--- src/app/pages/details/details.page.ts
+++ src/app/pages/details/details.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Share } from '@capacitor/share';
 import { NavController } from '@ionic/angular';
