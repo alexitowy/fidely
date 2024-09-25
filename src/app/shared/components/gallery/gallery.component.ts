330f335f0419e3817db8e773cdb3fa33961514ee
Otilia Nicola <otilianicola94@gmail.com>
2024-09-25 13:52:17 +0200

    fix: change gallery

--- src/app/shared/components/gallery/gallery.component.ts
+++ src/app/shared/components/gallery/gallery.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { DataCard } from 'src/app/core/interfaces/dataCard.interface';
-import { DataSwiper } from 'src/app/core/interfaces/dataSwiper.interface';
 
 @Component({
   selector: 'app-gallery',
@@ -10,7 +9,6 @@ import { DataSwiper } from 'src/app/core/interfaces/dataSwiper.interface';
 export class GalleryComponent implements OnInit {
   @Input() shop: DataCard;
 
-  img: DataSwiper[];
   configParams: any = {
     slidesPerView: 1,
     navigation: true,
@@ -20,18 +18,5 @@ export class GalleryComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit() {
-    this.img = [
-      {
-        id: '',
-        img: 'Baoba',
-        url: 'https://www.baobabelleza.com/wp-content/uploads/2024/06/pestanas_lifting_3D-300x300-1.png',
-      },
-      {
-        id: '',
-        img: 'Baoba',
-        url: 'https://www.baobabelleza.com/wp-content/uploads/2024/06/manicura-_-unas-gel-acrilico-300x300-1.jpg',
-      },
-    ];
-  }
+  ngOnInit() {}
 }
