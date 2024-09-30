75685241ecbb3ac1f0f6ced8fd7ca940b89aad24
Oti <otilianicola94@gmail.com>
2024-09-30 17:29:27 +0200

    fix: import router

--- src/app/pages/tabs/pages/view-search/view-search.page.ts
+++ src/app/pages/tabs/pages/view-search/view-search.page.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
 import { KeyStorage } from 'src/app/core/enums/localStorage.enum';
 import { CardShop } from 'src/app/core/interfaces/dataCard.interface';
@@ -10,7 +11,7 @@ import { LocalStorageService } from 'src/app/core/services/local-storage.service
   templateUrl: './view-search.page.html',
   styleUrls: ['./view-search.page.scss'],
 })
-export class ViewSearchPage implements OnInit {
+export class ViewSearchPage implements OnInit{
   configParams: any = {
     slidesPerView: 1,
     navigation: false,
@@ -49,7 +50,7 @@ export class ViewSearchPage implements OnInit {
           id: 'B1',
           icon: 'https://firebasestorage.googleapis.com/v0/b/fidelity-back.appspot.com/o/Icons%2Fimage-regular.svg?alt=media&token=19e15e71-c6f8-4754-888b-816c834f4628',
           title: 'Masaje',
-          subtitle: 'Baoba Belleza',
+          subtitle: '',
           desc: 'Masaje relajante de 30 minutos',
           currentCountService: '1',
           award: '1 servicio gratis',
@@ -102,22 +103,22 @@ export class ViewSearchPage implements OnInit {
     {
       id: '2',
       icon: 'https://firebasestorage.googleapis.com/v0/b/fidelity-back.appspot.com/o/Icons%2Ffacebook-brands-solid.svg?alt=media&token=cd543acd-7bf1-44a9-ae06-844773e961f3',
-      title: 'repsol',
+      title: 'Zaccha',
       cover:
       'https://firebasestorage.googleapis.com/v0/b/fidelity-back.appspot.com/o/fondoBaoba.webp?alt=media&token=44847806-40ef-48bf-806d-556c1143dd12',
-      subtitle: 'subtitle',
-      desc: 'venda de combustible',
+      subtitle: 'Alisado',
+      desc: 'Céntro Médico Estético de referencia en pleno corazón de la capital de España.',
       url: '',
       favorite: false,
-      categoryName: 'Medicina estética',
-      categoryColor: 'category1',
+      categoryName: 'Peluqueria',
+      categoryColor: 'category2',
       bonds: [
         {
           id: 'BA1',
           icon: 'https://firebasestorage.googleapis.com/v0/b/fidelity-back.appspot.com/o/Icons%2Fimage-regular.svg?alt=media&token=19e15e71-c6f8-4754-888b-816c834f4628',
-          title: 'Diesel',
-          subtitle: 'repsol diesel',
-          desc: 'Diesel normal, Diesel plus, Diesel max',
+          title: 'Corte',
+          subtitle: '',
+          desc: 'Corte de pelo para señora',
           currentCountService: '1',
           award: '1 servicio gratis',
           favorite: false,
@@ -150,16 +151,28 @@ export class ViewSearchPage implements OnInit {
 
   constructor(
     private readonly localStorageService: LocalStorageService,
-    private readonly navCtrl: NavController
+    private readonly navCtrl: NavController,
+    private readonly router: Router
   ) {}
+  ngOnInit(): void {
+    console.log('oninit');
+    
+  }
 
-  async ngOnInit() {
+  async ionViewWillEnter() {
+ 
+      console.log('Llegue ');
     this.lstFavoriteCards = (await this.localStorageService.get(KeyStorage.SHOPFAVORITES)) || [];
       
     await this.localStorageService.set(KeyStorage.SHOPS, this.cards);
   }
 
   toNavigate(card: CardShop) {
-    this.navCtrl.navigateForward(`/details/${card.id}`);
+    this.router.navigate([`/tabs/details/${card.id}`]);
+  }
+
+  ionViewWillLeave(){
+    console.log('saliendo');
+    
   }
 }
