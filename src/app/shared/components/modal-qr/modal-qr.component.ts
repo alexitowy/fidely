bece1356f7c1c581db179687461fe9b2b1341def
Oti <otilianicola94@gmail.com>
2024-10-01 17:03:41 +0200

    feat: create modal-qr

---
+++ src/app/shared/components/modal-qr/modal-qr.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-qr',
+  templateUrl: './modal-qr.component.html',
+  styleUrls: ['./modal-qr.component.scss'],
+})
+export class ModalQrComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
