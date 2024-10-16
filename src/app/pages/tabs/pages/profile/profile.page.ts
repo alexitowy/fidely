7cc0ab6ba4583c53bb2425934feb003c15eaed2c
Alex Guerrero <alex.guerrero@sngular.com>
2024-10-16 16:16:27 +0200

    fix: change form value and styles

--- src/app/pages/tabs/pages/profile/profile.page.ts
+++ src/app/pages/tabs/pages/profile/profile.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NavController } from '@ionic/angular';
+import { FirebaseAuthenticationService } from 'src/app/core/services/firebase-authentication.service';
+import { UtilsService } from 'src/app/core/services/utils.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,19 +10,53 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+  profileForm: FormGroup ;
 
-  profileForm: FormGroup = new FormGroup(
-    {
-    email: new FormControl('', [Validators.email, Validators.required]),
+  currentUser: any;
 
-    dateBirth: new FormControl('', Validators.required),
+  constructor(
+    private readonly firebaseAuthService: FirebaseAuthenticationService,
+    private readonly utilsService: UtilsService,
+    private readonly navCtrl: NavController
+  ) {}
 
-    phone: new FormControl('',[Validators.required, Validators.pattern(/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/g)]),
-  });
-
-  constructor() { }
-
-  ngOnInit() {
+  async ngOnInit() {
+  }
+  
+  async ionViewWillEnter(){
+    this.currentUser = await this.firebaseAuthService.getCurrentUser();
+    this.builderForm();
   }
 
+  builderForm(){
+    this.profileForm = new FormGroup({
+      name: new FormControl(
+        this.currentUser?.displayName ? this.currentUser.displayName : '', [
+        Validators.minLength(6),
+        Validators.maxLength(25),
+        Validators.required,
+      ]),
+      email: new FormControl(
+        this.currentUser?.email ?  this.currentUser.email : '', [Validators.email, Validators.required]),
+      dateBirth: new FormControl(null, Validators.required),
+      phone: new FormControl(this.currentUser?.phone ? this.currentUser.phone : '', [
+        Validators.required,
+        Validators.pattern(/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/g),
+      ]),
+    });
+  }
+
+  async onUpdate() {
+    console.log(this.profileForm.valid);
+    console.log(this.profileForm.value);
+    
+    
+    if (this.profileForm.valid) {
+      await this.firebaseAuthService.updateData(this.profileForm.controls['name'].value);
+      this.utilsService.presentToastSuccess('Datos actualizados');
+      this.navCtrl.navigateBack('/tabs/account');
+    } else {
+      this.profileForm.markAllAsTouched();
+    }
+  }
 }
